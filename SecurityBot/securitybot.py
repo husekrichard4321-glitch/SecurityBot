32165e840798b5111b5db0a8e3c5725b56802be9
husekrichard4321 <husekrichard4321@gmail.com>
2026-02-18 13:39:56 +0100

    First version of SecurityBot with functional sending

---
+++ SecurityBot/securitybot.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import tasks
+import random
+import os
+from dotenv import load_dotenv
+
+# Where is this script on the PC?
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# attaching a folder with a script
+MESSAGES_FILE = os.path.join(BASE_DIR, "zpravy.txt")
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = 1424102844969259142
+
+# authorization for bot
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+# reading and filtering file
+
+
+def load_messages():
+    if os.path.exists(MESSAGES_FILE):
+        with open(MESSAGES_FILE, "r", encoding="utf-8") as f:
+            return [line.strip() for line in f.readlines() if line.strip()]
+    else:
+        print(f"ERROR: File not found in path: {MESSAGES_FILE}")
+        return ["DEFAULT MESSAGE: The message file is missing "]
+
+# hourly loop and random message output
+
+
+@tasks.loop(seconds=10)
+async def hourly_message():
+    channel = client.get_channel(CHANNEL_ID)
+    if channel:
+        meessages = load_messages()
+        await channel.send(random.choice(meessages))
+
+# run the boot
+
+
+@client.event
+async def on_ready():
+    print(f"bot logged in as: {client.user.name}")
+    hourly_message.start()
+
+client.run(TOKEN)
